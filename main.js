d1b3e06f706b379f512d1a93dddf475fbd3d67ee
Qifan Zhang <zqf1904@gmail.com>
2016-05-07 16:23:59 -0400

    Added files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// You can get your username and user key from your Developer Portal
+// You must have already created and compiled a bot before you can talk to it
+var pb = new Pandorabot("aiaas.pandorabots.com", "1409612605655", "trumpdatingadvice", "a098f0f853a5f60d99d12dc2366bbfd2");
+function doTalk() {
+  var input = document.getElementById("yousay").value;
+  document.getElementById("yousay").value = "";
+  pb.talk(input, function(data) {
+    var response = data["responses"];
+    document.getElementById("response").innerHTML = response;
+    console.log(response);
+  });
+}
