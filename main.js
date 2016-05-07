bc0ad2b0dea5911092ae6385b64ef55e6f01eefe
Qifan Zhang <zqf1904@gmail.com>
2016-05-07 17:29:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function doTalk() {
   document.getElementById("yousay").value = "";
   pb.talk(input, function(data) {
     var response = data["responses"];
-      document.getElementById("response").innerHTML = old_html + "You: " + input+ "<br/>" + "Trump: " + response+ "<br/>";
+      document.getElementById("response").innerHTML = old_html + "You: " + input+ "<hr><hr/>" + "Trump: " + response+ "<hr><hr/>";
     console.log(response);
   });
 }
