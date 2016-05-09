8c532a47e3d084e4e42270824e860bf25729f670
Qifan Zhang <zqf1904@gmail.com>
2016-05-08 23:43:40 -0400

    Added files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function doTalk() {
   document.getElementById("yousay").value = "";
   pb.talk(input, function(data) {
     var response = data["responses"];
-      document.getElementById("response").innerHTML = old_html + "You: " + input+ "<br><br>" + "Trump: " + response+ "<hr>";
+      document.getElementById("response").innerHTML = old_html + "You: " + input+ "<br><br>" + "Trump: " + response + "<hr>";
     console.log(response);
     document.getElementById('answer').scrollTop = document.getElementById('answer').scrollHeight;
   });
